fa58c65497b90625d70ac6d6c9194e333db9c901
предложение <132725937+showjss@users.noreply.github.com>
2023-05-20 10:48:44 +0800

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,4 +1,5 @@
 from pygame import *
+font.init()
 window = display.set_mode((700, 500))
 display.set_caption('Пинг-понг')
 bg = transform.scale(image.load('bg.jpg'), (700, 500))
@@ -19,17 +20,24 @@ class Player(GameSprite):#создаем класс наследник
         keys_pressed = key.get_pressed()#получаем все события клавиатуры
         if keys_pressed[K_UP] and self.rect.y > 5:#если нажата кнопка влево и координаты больше 0 
             self.rect.y -= self.speed#тогда персонаж передвигается в лево
-        if keys_pressed[K_DOWN] and self.rect.y < 400: #если нажата кнопка в право и координаты меньше 600
+        if keys_pressed[K_DOWN] and self.rect.y < 350: #если нажата кнопка в право и координаты меньше 600
             self.rect.y += self.speed#тогда персонаж передвигаается в прав
     def update_l(self):# метод передвижение
         keys_pressed = key.get_pressed()#получаем все события клавиатуры
         if keys_pressed[K_w] and self.rect.y > 5:#если нажата кнопка влево и координаты больше 0 
             self.rect.y -= self.speed#тогда персонаж передвигается в лево
-        if keys_pressed[K_s] and self.rect.y < 400 : #если нажата кнопка в право и координаты меньше 600
+        if keys_pressed[K_s] and self.rect.y < 350: #если нажата кнопка в право и координаты меньше 600
             self.rect.y += self.speed#тогда персонаж передвигаается в прав
-racket1 = Player('racket.png', 10, 10, 5, 10, 100)
-racket2 = Player('racket.png', 680, 400, 5, 10, 100)
+ball = GameSprite('ball.png', 15, 10, 3, 50, 50)
+racket1 = Player('racket.png', 10, 10, 10, 10, 200)
+racket2 = Player('racket.png', 680, 300, 10, 10, 200)
+font1 = font.Font(None, 50)
+lose1 = font1.render('PLAYER 1 LOSE', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE', True, (180, 0, 0))
+speed_x = 5
+speed_y = 5
 game = True
+finish = False
 while game:
     window.blit(bg,(0, 0))
     for e in event.get():
@@ -39,4 +47,20 @@ while game:
     racket1.reset()
     racket2.update_r()
     racket2.reset()
+    ball.reset()
+    
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if ball.rect.y > 450 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (240, 220))
+    if ball.rect.x > 650:
+        finish = True
+        window.blit(lose2, (240, 220))
     display.update()
+    time.delay(8)
